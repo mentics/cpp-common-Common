94052fad8d9ba06653c60113c2c89747b3f6ef4d
jshellman <joel@mentics.com>
2017-07-13 02:55:55 -0700

    Compiles after adding threads to NetworkBase but not working yet

---
+++ include/MenticsCommonTest.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+
+namespace mentics { namespace test {
+
+void setupLog() {
+	auto sink = boost::log::add_file_log("unit-test.log");
+	sink->locked_backend()->auto_flush(true);
+	boost::log::core::get()->set_filter(
+		boost::log::trivial::severity >= boost::log::trivial::trace
+	);
+}
+
+}}
