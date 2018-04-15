a88baf7e3e6974c090130858a8d2d86f481ae373
jshellman <joel@mentics.com>
2018-04-14 17:06:48 -0700

    Updating lots of code and type stuff and formatting

--- include/MenticsCommon.h
+++ include/MenticsCommon.h
@@ -2,7 +2,6 @@
 #include "nn.hpp"
 #include "spdlog\spdlog.h"
 
-
 #define ONLY_MOVE(class_name) \
 public:\
 	class_name(class_name&&) = default;\
@@ -25,6 +24,10 @@ using TypeName##Ptr = nn::nn<TypeName<TemplateName>*>;\
 template <typename TemplateName>\
 using TypeName##UniquePtr = nn::nn<std::unique_ptr<TypeName<TemplateName>>>;
 
+//#define PTRS1_DEF(TypeName, TT)\
+//using TypeName##PtrT = nn::nn<TypeName<TT>*>;\
+//using TypeName##UniquePtrT = nn::nn<std::unique_ptr<TypeName<TT>>>;
+
 #define PTRS2(TypeName, TemplateName1, TemplateName2)\
 template <typename TemplateName1, typename TemplateName2>\
 using TypeName##Ptr = nn::nn<TypeName<TemplateName1, TemplateName2>*>;\
@@ -36,24 +39,22 @@ typedef uint8_t byte;
 namespace nn = dropbox::oxygen;
 
 namespace MenticsGame {
-	extern std::shared_ptr<spdlog::logger> mlog;
-	const std::string EMPTY_STRING;
-	uint64_t currentTimeMillis();
-	uint64_t currentTimeNanos();
 
-	// https://stackoverflow.com/questions/39912/how-do-i-remove-an-item-from-a-stl-vector-with-a-certain-value
+extern std::shared_ptr<spdlog::logger> mlog;
+const std::string EMPTY_STRING;
+uint64_t currentTimeMillis();
+uint64_t currentTimeNanos();
 
+template <typename T, typename... Args>
+nn::nn_unique_ptr<T> uniquePtr(Args &&... args) {
+	return nn::nn_unique_ptr<T>(nn::i_promise_i_checked_for_null,
+		std::unique_ptr<T>(new T(std::forward<Args>(args)...)));
+}
 
-	template <typename T, typename... Args>
-	nn::nn_unique_ptr<T> uniquePtr(Args &&... args) {
-		return nn::nn_unique_ptr<T>(nn::i_promise_i_checked_for_null,
-			std::unique_ptr<T>(new T(std::forward<Args>(args)...)));
-	}
-
-	template <typename T, typename TC, typename... Args>
-	nn::nn_unique_ptr<T> uniquePtrC(Args &&... args) {
-		return nn::nn_unique_ptr<T>(nn::i_promise_i_checked_for_null,
-			std::unique_ptr<T>(new TC(std::forward<Args>(args)...)));
-	}
+template <typename T, typename TC, typename... Args>
+nn::nn_unique_ptr<T> uniquePtrC(Args &&... args) {
+	return nn::nn_unique_ptr<T>(nn::i_promise_i_checked_for_null,
+		std::unique_ptr<T>(new TC(std::forward<Args>(args)...)));
+}
 
 }
